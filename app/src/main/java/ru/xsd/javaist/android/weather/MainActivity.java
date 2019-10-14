c2a688b726787ce8052aa4ca5c753ebeee79914e
Dmitry <javaist@xsd.ru>
2019-10-14 05:26:50 +0300

    Lesson 4: Answer (tiny fixes)

--- app/src/main/java/ru/xsd/javaist/android/weather/MainActivity.java
+++ app/src/main/java/ru/xsd/javaist/android/weather/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Switch;
